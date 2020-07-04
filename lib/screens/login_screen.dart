882cf7709d4f5d014031d333f586a63a2a6cc9c1
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2020-07-04 00:08:11 -0400

    Added sign in authentication and home screen placeholder

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 // import 'register_screen.dart';
+import 'home_screen.dart';
+import '../services/authentication.dart';
 import '../app/app_styles.dart';
 import '../app/app_strings.dart';
 import '../components/login_register_app_scaffold.dart';
@@ -7,15 +11,25 @@ import '../components/login_register_app_scaffold.dart';
 class LoginScreen extends StatefulWidget {
   LoginScreen({Key key}) : super(key: key);
 
+  static const appLogoPath = 'lib/assets/images/app_logo.png';
+  final AuthenticationManager auth = new AuthenticationManager();
+
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  static const appLogoPath = 'lib/assets/images/app_logo.png';
   final _formKey = GlobalKey<FormState>();
+  // var _loginSuccessful;
   var _email;
   var _password;
+  var _userId;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _isLoggedIn = false;
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +66,7 @@ class _LoginScreenState extends State<LoginScreen> {
     return Padding(
       padding: EdgeInsets.only(top: appLogoPadding()),
       child: Image(
-        image: AssetImage(appLogoPath),
+        image: AssetImage(LoginScreen.appLogoPath),
         height: appLogoHeight(),
       ),
     );
@@ -108,8 +122,19 @@ class _LoginScreenState extends State<LoginScreen> {
           color: AppStyles.primaryButtonColor,
           textColor: AppStyles.primaryButtonTextColor,
           child: Text('${AppStrings.loginButtonLabel}'),
-          onPressed: () {
-            if (_formKey.currentState.validate()) {}
+          onPressed: () async {
+            if (_formKey.currentState.validate()) {
+              _formKey.currentState.save();
+              logIn();
+              if (_userId == null) {
+                // todo: Add logic to display error message when login fails
+              } else {
+                pushHome();
+              }
+              // if (_userId != null) {
+              //   pushHome();
+              // }
+            }
           },
         ),
       ),
@@ -125,14 +150,6 @@ class _LoginScreenState extends State<LoginScreen> {
         },
       ),
     );
-  }
-
-  String validateEmail(String value) {
-    return value.isEmpty ? '${AppStrings.emailFieldHint}' : null;
-  }
-
-  String validatePassword(String value) {
-    return value.isEmpty ? '${AppStrings.passwordFieldHint}' : null;
   }
 
   double loginFormPadding() {
@@ -159,8 +176,30 @@ class _LoginScreenState extends State<LoginScreen> {
     return MediaQuery.of(context).size.height * 0.02;
   }
 
+  String validateEmail(String value) {
+    return value.isEmpty ? '${AppStrings.emailFieldHint}' : null;
+  }
+
+  String validatePassword(String value) {
+    return value.isEmpty ? '${AppStrings.passwordFieldHint}' : null;
+  }
+
+  void logIn() async {
+    // todo: revisit to determine if this is best way to implement
+    try {
+      _userId = await widget.auth.signIn(_email, _password);
+    } catch (e) {
+      _userId = null;
+    }
+  }
+
   // void pushRegister() {
   //   Navigator.push(
   //       context, MaterialPageRoute(builder: (context) => RegisterScreen()));
   // }
+
+  void pushHome() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => HomeScreen()));
+  }
 }
