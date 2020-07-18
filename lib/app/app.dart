82ff230b99c53ff4c5557ca2fea909534c3a8191
mdhen <hennigam@oregonstate.edu>
2020-07-18 12:37:49 +0700

    added routes table to MaterialApp

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,9 @@
+import 'dart:js';
+
+import 'package:bike_kollective/screens/active_screen.dart';
+import 'package:bike_kollective/screens/addBike_screen.dart';
+import 'package:bike_kollective/screens/home_screen.dart';
+import 'package:bike_kollective/screens/register_screen.dart';
 import 'package:flutter/material.dart';
 import 'app_strings.dart';
 import '../screens/login_screen.dart';
@@ -9,6 +15,16 @@ class MyApp extends StatelessWidget {
       title: AppStrings.appTitle,
       home: LoginScreen(),
       theme: ThemeData(canvasColor: Colors.white),
+      initialRoute: '/',
+      routes: {
+      '/': (context) => HomeScreen(),
+      '/login': (context) => LoginScreen(),
+      '/registration': (context) => RegisterScreen(),
+      '/add': (context) => AddBikeScreen(),
+      //'/bikeDetails': (context) => BikeDetailsScreen(),
+      '/active' : (context) => ActiveScreen(),
+      //'/rate' : (context) => RateScreen(),
+      },    
     );
   }
 }
