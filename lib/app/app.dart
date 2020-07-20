b612d253419c5003c54f07d5ba81bea3b423190e
mdhen <hennigam@oregonstate.edu>
2020-07-20 07:50:33 +0700

    list screen semi functional

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:bike_kollective/screens/active_screen.dart';
 import 'package:bike_kollective/screens/addBike_screen.dart';
 import 'package:bike_kollective/screens/home_screen.dart';
+import 'package:bike_kollective/screens/list_screen.dart';
 import 'package:bike_kollective/screens/register_screen.dart';
 import 'package:flutter/material.dart';
 import 'app_strings.dart';
@@ -11,14 +12,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: AppStrings.appTitle,
-      //home:AddBikeScreen(),
       theme: ThemeData(canvasColor: Colors.white),
       initialRoute: '/',
       routes: {
-      '/': (context) => AddBikeScreen(),
+      '/': (context) => ListScreen(),
       '/login': (context) => LoginScreen(),
       '/registration': (context) => RegisterScreen(),
       '/add': (context) => AddBikeScreen(),
+      '/list': (context) => ListScreen(), 
       //'/bikeDetails': (context) => BikeDetailsScreen(),
       '/active' : (context) => ActiveScreen(),
       //'/rate' : (context) => RateScreen(),
