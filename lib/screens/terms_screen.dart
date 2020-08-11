89f2bacd535444ee3b915805ba8386724d33c6e2
Matthew Hennigan <mdhen@Matthews-MBP.fritz.box>
2020-08-11 08:56:04 +0200

    added terms screen

--- lib/screens/terms_screen.dart
+++ lib/screens/terms_screen.dart
@@ -1,11 +1,5 @@
 import 'dart:io';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter/material.dart';
-import 'package:location/location.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:path/path.dart' as Path;
 import '../app/app_styles.dart';
 
 class AddBikeScreen extends StatefulWidget {
@@ -20,51 +14,33 @@ class AddBikeScreen extends StatefulWidget {
 }
 
 class _AddBikeScreenState extends State<AddBikeScreen> {
-  var _scaffoldContext;
-  File _image;
-  final picker = ImagePicker();
-
-  final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
-
-  Firestore firestore = Firestore.instance;
-  CollectionReference bikes = Firestore.instance.collection('bikes');
-
-  Location location = new Location();
-  PermissionStatus _permissionGranted;
-  LocationData _locationData;
-  bool _serviceEnabled;
-
-  // JSON categorie variables
-  String inputType, inputSize, inputCombo, uploadedFileURL;
-  var _documentID;
-  double inputLat, inputLng, rating;
-  int ratingNum;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppStyles.primaryScaffoldBackgroundColor,
-      body: Builder(builder: (BuildContext context) {
-        _scaffoldContext = context;
-        return SafeArea(
-            child: SingleChildScrollView(
+      body: SafeArea(
+        child: SingleChildScrollView(
           child: Column(
             children: [
               _terms(),
-              _formBuilder(),
               _submitButton(),
             ],
           ),
-        ));
-      }),
+        ),
+      ),
     );
   }
 
   Widget _terms() {
-    return Column();
+    return Text(
+        'TERMS OF USE - You agree not to steal any bikes or sue us if you get injured');
   }
 
-  Widget _formBuilder() {}
-
-  Widget _submitButton() {}
+  Widget _submitButton() {
+    return RaisedButton(
+        child: Text("Continue"),
+        onPressed: () {
+          Navigator.pop(context);
+        });
+  }
 }
